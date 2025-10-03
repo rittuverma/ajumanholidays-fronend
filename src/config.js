d58e2092dca984ded885835daf62a7e5edaa274c
Ritu Verma <77578748+rittuverma@users.noreply.github.com>
2025-10-03 19:46:31 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,6 @@
-export const API_URL = import.meta.env.REACT_APP_API_URL || "http://localhost:5000";
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? process.env.REACT_APP_API_URL
+    : "http://localhost:5000";
+
 
