3880778b91d569ea1e186b724d55ec039ba80c5e
Ritu Verma <77578748+rittuverma@users.noreply.github.com>
2025-10-03 16:28:23 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-// export const API_URL =
-//   process.env.NODE_ENV === "production"
-//     ? "https://your-deployed-backend.com"   // deployed backend
-//     : "http://localhost:5000";              // local dev backend
-export const API_URL = "http://localhost:5000"; // if backend runs on port 5000
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://your-deployed-backend.com"   // deployed backend
+    : "http://localhost:5000";              // local dev backend
+// export const API_URL = "http://localhost:5000"; // if backend runs on port 5000
